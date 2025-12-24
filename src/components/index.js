bf939834dff8ae35dc93084db49a980f170b9ca5
Murat Keskin <danteagreil@gmail.com>
2025-12-24 20:56:54 +0300

    implement dock presentation

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/welcome.jsx";
+import Dock from "#components/Dock.jsx";
 
-export { Navbar, Welcome };
+export { Navbar, Welcome, Dock };
